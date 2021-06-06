d9fec3015a3d85a65255893431ad040890e738bf
Yaritza <yaritzamicaelleb@gmail.com>
2021-06-06 19:38:18 -0300

    Tarefa Fibonacci?

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroAnterior = 0;
+            int numeroAtual = 1;
+            int novoNumero;
+            int fibonacci;
+
+   
+           for (int i = 0; i <100;  i++ )
+            {  
+            fibonacci = numeroAnterior + numeroAtual; 
+            Console.WriteLine  (fibonacci);
+            numeroAnterior = numeroAtual; 
+            numeroAtual = fibonacci;   
+            }
+
+        }
+    }
+}
