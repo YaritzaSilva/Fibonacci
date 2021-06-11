4142ce7d9490b1a145bcbbdd46b3643642c0a674
Diego Neri <diego@diegoneri.com>
2021-06-11 18:40:52 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Fibonacci
             int fibonacci = 0;
 
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine(fibonacci);
                 numeroAnterior = numeroAtual;
